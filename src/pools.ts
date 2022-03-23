346ad487619336392a36c0c80cef4ca5cf9aeba5
Danylo <danylo@code-care.pro>
2022-03-23 23:23:05 +0200

    init ropsten

--- src/pools.ts
+++ src/pools.ts
@@ -1,3 +1,3 @@
 import { toLowerCase } from './helpers'
 
-export const pools: string[] = ['0x503828976d22510aad0201ac7ec88293211d23da'].map(toLowerCase)
+export const pools: string[] = ['0x08E194787b65f86AA3C2990F7F009E9603Bbff25'].map(toLowerCase)
